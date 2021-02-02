95db0f3e6a65658bdc68607e99db0864e59eab35
r4hr <info@d4hr.com>
2021-02-02 17:32:46 -0300

    Inicia análisis exploratorio

--- surv_hrdataset.R
+++ surv_hrdataset.R
@@ -7,7 +7,7 @@ library(survival)
 library(survivalROC)
 library(lubridate)
 library(caret)
-
+library(DataExplorer)
 
 # Datos --------------------------------------
 
@@ -21,15 +21,17 @@ status(hrdata)
 
 summary(hrdata)
 
+# Hay variables que se repiten (codificación y detalle). 
+# Busco los nombres para simplificar
 variables <- names(hrdata)
 sort(variables)
 
-# Elimino variables redundantes (ID, por ej.) o innecesarias para el análisis
+# Elimino variables redundantes 
 hrdata1 <- hrdata %>% 
-  select(EmpID, Employee_Name, DateofHire, DateofTermination, Position, DOB, 
-         EmploymentStatus, EmpSatisfaction, EngagementSurvey, ManagerID, 
+  select(EmpID, Employee_Name, DateofHire, Department, DateofTermination, Position, DOB, 
+         EmploymentStatus, EmpSatisfaction, EngagementSurvey, ManagerName, 
          MaritalDesc, PayRate, PerformanceScore, RecruitmentSource, Sex, 
-         SpecialProjectsCount, Termd, TermReason, DaysLateLast30)
+         SpecialProjectsCount, TermReason, DaysLateLast30, Termd)
 
 # Cambiar los campos de fecha de chr a date
 hrdata1 <- hrdata1 %>% 
@@ -40,6 +42,81 @@ hrdata1 <- hrdata1 %>%
          antiguedad = as.period(interval(start = DateofHire, end = "2017-06-30"))$year)
 
 summary(hrdata1)
+
+
+# Análisis Exploratorio --------------
+
+freq(hrdata1)
+
+profiling_num(hrdata1)
+
+DataExplorer::create_report(hrdata1, y = "Termd")
+
+hrdata1 <- hrdata1 %>% 
+  select(-DaysLateLast30) %>% 
+  rename(target = Termd) %>% 
+  mutate(Department = str_trim(Department, side = "both"))
+
+status(hrdata1)
+
+
+hrdata1 %>% 
+  filter(!is.na(DateofTermination)) %>% 
+  group_by(Department) %>% 
+  summarise(cantidad = n()) %>% 
+  ggplot(aes(x = reorder(Department, cantidad),
+             y = cantidad)) +
+  geom_col() +
+  geom_text(aes(label=cantidad,
+                vjust=-0.2)) +
+  labs(title = "Empleados por Departamento",
+       x = "", y = "") +
+  scale_x_discrete(guide = guide_axis(n.dodge = 2))
+
+
+hrdata1 %>% 
+  group_by(Department) %>% 
+  summarise(turnover_rate = mean(target),
+            bajas = sum(target))
+
+# Production es el sector con más cantidad de bajas, nominales y proporcionales.
+hrdata1 %>% 
+  filter(Department == "Production") %>% 
+  group_by(Position, target) %>% 
+  tally()
+
+
+# Analizar la evolución de las bajas y altas.
+hrdata1 <- hrdata1 %>% 
+  mutate(year_hire = year(floor_date(DateofHire)),
+         year_term = year(floor_date(DateofTermination)))
+
+
+
+hires <- hrdata1 %>%  
+  filter(Department == "Production") %>% 
+  group_by(year = year_hire, Position) %>% 
+  summarise(ingresos = n()) 
+  
+terms <- hrdata1 %>% 
+  filter(!is.na(year_term), Department == "Production") %>% 
+  group_by(year = year_term, Position) %>% 
+  summarise(egresos = n()) 
+
+summary(hires)
+summary(terms)
+
+
+turnover <- rbind(hires, terms) %>% arrange(year)
+
+turnover <- turnover %>% 
+  pivot_longer(cols = c(ingresos, egresos), names_to = "movimiento", 
+               values_to = "cantidad") %>% 
+  mutate(cantidad = replace_na(cantidad, replace = 0))
+
+ggplot(turnover, aes(x = year, y = cantidad, color = movimiento)) +
+  geom_line() +
+  facet_wrap(~Position, scales = "free_y")
 
 
 # Primer modelo -------------------------
